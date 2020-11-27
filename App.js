78237d5e0477851de698c1483141d1292d42283d
Wasiq Qamar <wasiq.qbutt@gmail.com>
2020-11-27 19:59:34 +0500

    Price and Discount Field Added

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [price, onChangePrice] = useState("");
+  const [discount, onChangeDiscount] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Maula JAtt aaaaaaaaaaaaaaaaaaaa</Text>
+      <TextInput 
+      style={styles.input} 
+      value={price}
+      placeholder="Original Price"
+      onChangeText={text => onChangePrice('Rs.' + text)} />
+      <TextInput 
+      style={styles.input} 
+      value={discount}
+      placeholder="Discount Rate"
+      onChangeText={text => onChangeDiscount(text + '%')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +29,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    padding: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    width: 300
+  }
 });
