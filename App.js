d25844405f1142e5ed466541a1d51eecb2f8cd2b
Wasiq Qamar <wasiq.qbutt@gmail.com>
2020-11-27 21:45:23 +0500

    Money Saved and Final Price added

--- App.js
+++ App.js
@@ -1,23 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React , {useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [price, onChangePrice] = useState("");
   const [discount, onChangeDiscount] = useState("");
+
+  let savedMoney ;
+  let finalPrice ;
+
+  const calcDiscount = () => {
+    const localPrice = eval(price);
+    const localDiscount = eval(discount)
+    if (price!=="" && discount!==""){
+      savedMoney = localPrice/100 * localDiscount;
+      finalPrice = localPrice - savedMoney;
+      savedMoney = "Money Saved:   Rs."+savedMoney;
+      finalPrice = "Final Price:        Rs."+finalPrice;
+    }
+  }
+
   return (
     <View style={styles.container}>
       <TextInput 
       style={styles.input} 
       value={price}
       placeholder="Original Price"
-      onChangeText={text => onChangePrice('Rs.' + text)} />
+      onChangeText={text => onChangePrice(text)} />
       <TextInput 
       style={styles.input} 
       value={discount}
       placeholder="Discount Rate"
-      onChangeText={text => onChangeDiscount(text + '%')} />
+      onChangeText={text => onChangeDiscount(text)} />
       <StatusBar style="auto" />
+      {calcDiscount()}
+      <View>
+        <Text>{savedMoney}</Text>
+        <Text>{finalPrice}</Text>
+      </View>
     </View>
   );
 }
